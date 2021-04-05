275852a46237210c9d30dece4e31c74ab43a7a93
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-04-06 00:33:31 +0530

    refactor-refactored data model

--- src/api/mock-server.js
+++ src/api/mock-server.js
@@ -11,80 +11,19 @@ export const PixWebServer=()=>{
             application: RestSerializer
         },
         models: {
-            basicCameraUsage:Model,
-            cameraMaintaince:Model,
-            coolCameraTrick:Model,
-            nightPhotography:Model,
-            composition:Model,
-            astroPhotography:Model
+            fullVideosList:Model,
         },
         seeds(server){
-            basicCameraUsageData.forEach(({name,link,catagory,author})=>(
-                server.create("basicCameraUsage",{
+            
+            [...basicCameraUsageData,...cameraMaintainanceData,...coolCameraTricksData,
+            ...nightPhotographyData,...compositionData,...astroPhotographyData]
+            .forEach(({name,link,catagory,author,catagoryId})=>(
+                server.create("fullVideosList",{
                     id:faker.datatype.uuid(),
                     name:name,
                     link:link,
                     catagory:catagory,
-                    author:author,
-                    description:faker.commerce.productDescription(),
-                    rating:faker.datatype.float({'min':3,'max':5}).toFixed(1),
-                    recomended:faker.datatype.boolean()
-                })
-            ))
-            cameraMaintainanceData.forEach(({name,link,catagory,author})=>(
-                server.create("cameraMaintaince",{
-                    id:faker.datatype.uuid(),
-                    name:name,
-                    link:link,
-                    catagory:catagory,
-                    author:author,
-                    description:faker.commerce.productDescription(),
-                    rating:faker.datatype.float({'min':3,'max':5}).toFixed(1),
-                    recomended:faker.datatype.boolean()
-                })
-            ))
-            coolCameraTricksData.forEach(({name,link,catagory,author})=>(
-                server.create("coolCameraTrick",{
-                    id:faker.datatype.uuid(),
-                    name:name,
-                    link:link,
-                    catagory:catagory,
-                    author:author,
-                    description:faker.commerce.productDescription(),
-                    rating:faker.datatype.float({'min':3,'max':5}).toFixed(1),
-                    recomended:faker.datatype.boolean()
-                })
-            ))
-            nightPhotographyData.forEach(({name,link,catagory,author})=>(
-                server.create("nightPhotography",{
-                    id:faker.datatype.uuid(),
-                    name:name,
-                    link:link,
-                    catagory:catagory,
-                    author:author,
-                    description:faker.commerce.productDescription(),
-                    rating:faker.datatype.float({'min':3,'max':5}).toFixed(1),
-                    recomended:faker.datatype.boolean()
-                })
-            ))
-            compositionData.forEach(({name,link,catagory,author})=>(
-                server.create("composition",{
-                    id:faker.datatype.uuid(),
-                    name:name,
-                    link:link,
-                    catagory:catagory,
-                    author:author,
-                    description:faker.commerce.productDescription(),
-                    rating:faker.datatype.float({'min':3,'max':5}).toFixed(1),
-                    recomended:faker.datatype.boolean()
-                })
-            ))
-            astroPhotographyData.forEach(({name,link,catagory,author})=>(
-                server.create("astroPhotography",{
-                    id:faker.datatype.uuid(),
-                    name:name,
-                    link:link,
-                    catagory:catagory,
+                    catagoryId:catagoryId,
                     author:author,
                     description:faker.commerce.productDescription(),
                     rating:faker.datatype.float({'min':3,'max':5}).toFixed(1),
@@ -95,12 +34,8 @@ export const PixWebServer=()=>{
         routes(){
             this.namespace = "api";
             this.timing = 1000;
-            this.get("/basic-camera-usage", schema=>schema.basicCameraUsages.all());
-            this.get("/camera-maintainance", schema=>schema.cameraMaintainces.all());
-            this.get("/cool-camera-tricks", schema=>schema.coolCameraTricks.all());
-            this.get("/night-photography", schema=>schema.nightPhotographies.all())
-            this.get("/composition", schema=>schema.compositions.all())
-            this.get("/astro-photography", schema=>schema.astroPhotographies.all())
+            this.get("/load-all-videos", schema=>schema.fullVideosLists.all());
+            
         }
     })
 }
