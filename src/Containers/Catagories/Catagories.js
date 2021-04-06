5cd3d1e2a0b3e6e875120844ced0220bd0a9d43d
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-04-06 16:18:10 +0530

    feature-added video selection and play feature

--- src/Containers/Catagories/Catagories.js
+++ src/Containers/Catagories/Catagories.js
@@ -1,2 +1,37 @@
 import classes from './Catagories.module.css';
+import {useParams} from 'react-router-dom'
+import {useContext,useEffect} from 'react'
+import {CatagoriesContext} from '../../Store/Catagories-context-reducer'
+import VideoNameList from '../VideoNameList/VideoNameList'
+import Youtube from 'react-youtube'
 
+const Catagories=()=>{
+    const {id}=useParams()
+    const {dispatch,selectedVideo}=useContext(CatagoriesContext);
+
+    useEffect(()=>{
+        dispatch({
+            type:"FILTER_VIDEO_BY_CATAGORY",
+            payload:id
+        })
+    },[id,dispatch])
+
+
+    return(
+        <div className={classes["catagory-container"]}>
+            <div className={classes["catagory-video-name-list"]}>
+                <VideoNameList/>
+            </div>
+            <div className={classes["catagory-video-play-area"]}>
+                {selectedVideo&&<Youtube
+                    videoId={selectedVideo.link}
+                    className={classes["youtube-window"]}
+                    onPause={event=>console.log(event.target.playerInfo.currentTime)}
+                />}
+            </div>
+        </div>
+    )
+
+}
+
+export default Catagories
