44df5f6bc8b949739aafcc945d93c5496a4436a6
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-05-30 19:45:06 +0530

    Update useAxios.js

--- src/useAxios.js
+++ src/useAxios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const instance=axios.create({
-    baseURL:"https://pixweb-api.herokuapp.com/"
-})
+const instance = axios.create({
+  baseURL: "https://pixweb-api.herokuapp.com/",
+});
 
-export default instance
+export default instance;
